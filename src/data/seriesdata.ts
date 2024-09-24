0d9b77661fdfc91c8ba46105d33cac612990423a
Majorosas16 <mariajose.rosascuellar@gmail.com>
2024-09-24 11:38:23 -0500

    data

---
+++ src/data/seriesdata.ts
@@ -0,0 +1,336 @@
+const seriesdata = [
+    {
+        id: 1,
+        category: 'serie',
+        name: 'Grey\'s Anatomy',
+        description: 'A drama that focuses on the character of Merdith Grey, a member of a group of young doctors at the Seattle hospital. The daughter of a famous surgeon, who struggles to maintain her relationships with her colleagues.',
+        releaseDateFirstEpisode: 
+        {
+            month: "March",
+            year: 2005
+        },
+        createdBy: 'Shonda Rhimes',
+        seasons: '20',
+        episodes: '430',
+        genre: 'Medical drama',
+        ageRate: '12+',
+        trailer: 'https://www.youtube.com/watch?v=dOFJiFOwFuM',
+        images:
+        {
+            poster1: 'https://ew.com/thmb/rbVxqfE_wtj9ZeMNWWPWQ_ndvvI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Greys-Anatomy-2fd58eeedd03410f9cdd2fae962f7f1c.jpg',
+            poster2: 'https://i.pinimg.com/originals/5c/c7/9c/5cc79c58145bef3fde3c441e6fd12bfc.jpg',
+            poster3: 'https://i.redd.it/tyj7vee7xtq91.png',
+            posterH: 'https://lovingseries.com/wp-content/uploads/2023/03/greys-anatomy-abc.jpg',
+        }
+    },
+    {
+        id: 2,
+        category: 'serie',
+        name: 'Dahmer – Monster: The Jeffrey Dahmer Story',
+        description: 'A true-crime drama that tells the story of the infamous serial killer Jeffrey Dahmer, focusing on his gruesome murders and the failures of law enforcement to stop him.',
+        releaseDateFirstEpisode: {
+
+            month: 'September',
+            year: 2022
+        },
+        createdBy: 'Ryan Murphy, Ian Brennan',
+        seasons: '1',
+        episodes: '10',
+        genre: 'Crime drama, Thriller',
+        ageRate: '18+',
+        trailer: 'https://www.youtube.com/watch?v=1PgKjIosF50',
+        images: {
+            poster1: 'https://www.talonmarks.com/wp-content/uploads/2022/10/dahmer-poster.jpg',
+            poster2: 'https://fiu-original.b-cdn.net/fontsinuse.com/use-images/N183/183607/183607.jpeg?filename=FdOpvARXwAEgQ6J.jpeg',
+            posterH: 'https://www.sdpnoticias.com/resizer/v2/5R5YE3SDUFDGNKWYVEPTJLGCVA.webp?smart=true&auth=76325e6fbfa7b1aced31809a4341baee13e0a24ce816d2049fcdacf9d4f44753&width=1440&height=810',
+        }
+    },
+    {
+        id: 3,
+        category: 'serie',
+        name: 'Obi-Wan Kenobi',
+        description: 'A Star Wars series that follows Jedi Master Obi-Wan Kenobi as he hides from the Empire while keeping a close watch on a young Luke Skywalker, taking place between the events of Episodes III and IV.',
+
+        releaseDateFirstEpisode: {
+            month: 'May',
+            year: 2022
+        },
+        createdBy: 'Joby Harold',
+        seasons: '1',
+        episodes: '6',
+        genre: 'Science fiction, Adventure',
+        ageRate: '14+',
+        trailer: 'https://www.youtube.com/watch?v=3Yh_6_zItPU',
+        images: {
+            poster1: 'https://i.etsystatic.com/37268737/r/il/961f35/4469956211/il_fullxfull.4469956211_1545.jpg',
+            poster2: 'https://i.ebayimg.com/00/s/MTYwMFgxMDY2/z/4o0AAOSwR5dikRad/$_57.JPG?set_id=880000500F',
+            poster3: 'https://i.pinimg.com/736x/90/5c/ba/905cba584431106167f6e3aff88ddf38.jpg',
+            posterH: 'https://i0.wp.com/thefutureoftheforce.com/wp-content/uploads/2022/05/Obi-Wan-Kenobi-Disney-Plus-Keyart-Header.jpg?fit=1932%2C1092&ssl=1',
+        }
+    },
+    {
+        id: 4,
+        category: 'serie',
+        name: 'Arcane',
+        description: 'Set in the universe of the League of Legends video game, Arcane tells the origin stories of characters from Piltover and the undercity of Zaun, focusing on the tension between the two cities.',
+        releaseDateFirstEpisode: {
+            month: 'November',
+            year: 2021
+        },
+        createdBy: 'Christian Linke, Alex Yee',
+        seasons: '1',
+        episodes: '9',
+        genre: 'Animation, Action, Adventure',
+        ageRate: '16+',
+        trailer: 'https://www.youtube.com/watch?v=fXmAurh012s',
+        images: {
+            poster1: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/09/arcane-temporada-2-jinx-vi-4121695.jpg?tf=3840x',
+            poster2: 'https://cdn.clarosports.com/clarosports/2024/06/arcane-2-132145-819x1024.jpg',
+            poster3: 'https://doubletoasted.com/wp-content/uploads/2021/12/Arcane-Poster.jpg',
+            posterH: 'https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2024/06/imagen_2024-06-03_135525728.jpg',
+        }
+    },
+    {
+        id: 5,
+        category: 'serie',
+        name: 'Moon Knight',
+        description: 'A Marvel series about Marc Spector, a mercenary with dissociative identity disorder, who is drawn into a deadly mystery involving Egyptian gods.',
+
+        releaseDateFirstEpisode: {
+            month: 'March',
+            year: 2022
+        },
+        createdBy: 'Jeremy Slater',
+        seasons: '1',
+        episodes: '6',
+        genre: 'Superhero, Fantasy',
+        ageRate: '16+',
+        trailer: 'https://www.youtube.com/watch?v=4QIbLY2ZOAU',
+        images: {
+            poster1: 'https://artofthemovies.co.uk/cdn/shop/products/moon-knight_xgjz7xk2_1024x1024@2x.jpg?v=1659543021',
+            poster2: 'https://sportshub.cbsistatic.com/i/2022/01/18/4be0781d-8066-4a67-a7c5-4978687f05a1/moon-knight-poster.jpg?auto=webp&width=864&height=1280&crop=0.675:1,smart',
+            poster3: 'https://i.pinimg.com/736x/59/f2/48/59f24859a789832d7626c755f03cc6b5.jpg',
+            posterH: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2023/10/moon-knight-season-1-mcu-poster.jpg',
+        }
+    },
+    {
+        id: 6,
+        category: 'serie',
+        name: 'Gen V',
+        description: 'A spin-off from The Boys, Gen V follows young superheroes as they attend a college run by Vought International, training to become future heroes while navigating the complexities of adolescence.',
+        releaseDateFirstEpisode: {
+            month: 'September',
+            year: 2023
+        },
+        createdBy: 'Michele Fazekas, Tara Butters',
+        seasons: '1',
+        episodes: '8',
+        genre: 'Superhero, Action',
+        ageRate: '18+',
+        trailer: 'https://www.youtube.com/watch?v=UHJYxZrjU7s',
+        images: {
+            poster1: 'https://image.tmdb.org/t/p/original/uuot1N5AgZ7xRCKgm4ZCwOhgIJu.jpg',
+            poster2: 'https://i.ebayimg.com/images/g/gOAAAOSwrYxlRhJ0/s-l1200.jpg',
+            poster3: 'https://posterspy.com/wp-content/uploads/2023/10/GENV-dark.jpg',
+            posterH: 'https://assets.aboutamazon.com/0e/cb/ff25b4dd484da76277c5c402a96e/byso-s1-prposterframe-ensemble-1280x720-evrgrn-final-en-us-prico.jpg',
+        }
+    },
+    {
+        id: 7,
+        category: 'serie',
+        name: 'The Boys',
+        description: 'A group of vigilantes, known informally as "The Boys", set out to take down corrupt superheroes who abuse their powers for personal gain.',
+        releaseDateFirstEpisode: {
+            month: 'July',
+            year: 2019
+        },
+        createdBy: 'Eric Kripke',
+        seasons: '4',
+        episodes: '33',
+        genre: 'Superhero, Action, Comedy',
+        ageRate: '18+',
+        trailer: 'https://www.youtube.com/watch?v=RzDdLdM59kw',
+        images: {
+            poster1: 'https://m.media-amazon.com/images/I/81u7-vDBYrL.jpg',
+            poster2: 'https://i0.wp.com/latorrenews.com/wp-content/uploads/2024/05/image-68.png?fit=1080%2C1350&ssl=1',
+            poster3: 'https://cdn.clarosports.com/clarosports/2024/05/image-105834-819x1024.png',
+            posterH: 'https://m.media-amazon.com/images/I/91RcNFJDSFL.jpg',
+        }
+    },
+    {
+        id: 8,
+        category: 'serie',
+        name: 'Betty la fea (Yo soy Betty, la fea)',
+        description: 'A Colombian telenovela about an unattractive but brilliant economist who gets a job at a fashion company, where she faces numerous challenges and eventually rises to power.',
+        releaseDateFirstEpisode: {
+            month: 'October',
+            year: 1999
+        },
+        createdBy: 'Fernando Gaitán',
+        seasons: '1',
+        episodes: '335',
+        genre: 'Telenovela, Comedy, Romance',
+        ageRate: '10+',
+        trailer: 'https://www.youtube.com/watch?v=xTO-rrvZ9Dc',
+        images: {
+            poster1: 'https://image.tmdb.org/t/p/original/vIrR82OKssM5KkSJu3AATEXFrlC.jpg',
+            poster2: 'https://larepublica.cronosmedia.glr.pe/migration/images/LLA5DSPPHFDE7PK2IEFQHO3WUM.jpg',
+            posterH: 'https://m.media-amazon.com/images/S/pv-target-images/b55b8807ca03e0c2e01aac6710aa1ecf0c2039d973a61835907b7382301eb323.jpg',
+        }
+    },
+    {
+        id: 9,
+        category: 'serie',
+        name: 'Hawkeye',
+        description: 'A Marvel series where Clint Barton, also known as Hawkeye, teams up with young archer Kate Bishop to face enemies from his past and secure his legacy as a hero.',
+        releaseDateFirstEpisode: {
+            month: 'November',
+            year: 2021
+        },
+        createdBy: 'Jonathan Igla',
+        seasons: '1',
+        episodes: '6',
+        genre: 'Superhero, Action',
+        ageRate: '12+',
+        trailer: 'https://www.youtube.com/watch?v=5VYb3B1ETlk',
+        images: {
+            poster1: 'https://i.etsystatic.com/18242346/r/il/1ade4a/3639880697/il_fullxfull.3639880697_qkzu.jpg',
+            poster2: 'https://www.vintagemovieposters.co.uk/wp-content/uploads/2022/04/IMG_6131-scaled.jpeg',
+            poster3: 'https://image.tmdb.org/t/p/original/u66riEDVe2XPGWWZcbj2YGPLwoj.jpg',
+            posterH: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/12/Hawkeye-Season-Finale-Poster-Marvel-Studios-Disney-Plus.jpg   ',
+        }
+    },
+    {
+        id: 10,
+        category: 'serie',
+        name: 'Emily in Paris',
+        description: 'A romantic comedy series following Emily Cooper, an ambitious marketing executive from Chicago, who unexpectedly lands her dream job in Paris and faces challenges adapting to French culture.',
+        releaseDateFirstEpisode: {
+            month: 'October',
+            year: 2020
+        },
+        createdBy: 'Darren Star',
+        seasons: '4',
+        episodes: '40',
+        genre: 'Comedy, Romance, Drama',
+        ageRate: '14+',
+        trailer: 'https://www.youtube.com/watch?v=lptctjAT-Mk',
+        images: {
+            poster1: 'https://miro.medium.com/v2/resize:fit:1080/1*JwKozXiYZmAVQRFPUnItqA.jpeg',
+            poster2: 'https://m.media-amazon.com/images/M/MV5BODI5Y2YxM2UtZjhjYy00ZjM0LTg3NjQtYjQxMTBmZjM4ZTlkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+            poster3: 'https://images.squarespace-cdn.com/content/v1/586b14ac579fb3ccd8a82a19/4c99114c-2266-4627-a4d7-8340452350bf/Emily+in+Paris.jpeg',
+            posterH: 'https://hips.hearstapps.com/hmg-prod/images/emily-in-paris-new-snaps-669e5fb50e4b4.png?crop=1xw:0.7969452773613194xh;center,top&resize=1200:*',
+        }
+    },
+    {
+        id: 11,
+        category: 'serie',
+        name: 'The Umbrella Academy',
+        description: 'A dysfunctional family of adopted superhero siblings reunite to solve the mystery of their father’s death and prevent an impending apocalypse.',
+        releaseDateFirstEpisode: {
+            month: 'February',
+            year: 2019
+        },
+        createdBy: 'Steve Blackman, Jeremy Slater',
+        seasons: '4',
+        episodes: '40',
+        genre: 'Superhero, Action, Fantasy',
+        ageRate: '16+',
+        trailer: 'https://www.youtube.com/watch?v=s0LVj0yo308',
+        images: {
+            poster1: 'https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2024/07/GR92j6RWMAIwJml.jpg',
+            poster2: 'https://static0.cbrimages.com/wordpress/wp-content/uploads/2023/08/the-umbrella-academy-netflix-poster.jpg',
+            poster3: 'https://m.media-amazon.com/images/I/81t7Wbd18dL.jpg',
+            posterH: 'https://sm.ign.com/t/ign_latam/news/t/the-umbrel/the-umbrella-academy-new-poster-includes-season-2-easter-egg_ttsk.1200.jpg',
+        }
+    },
+    {
+        id: 12,
+        category: 'serie',
+        name: 'Stranger Things',
+        description: 'A group of kids in a small town uncover a series of supernatural mysteries involving secret government experiments, alternate dimensions, and telekinetic powers.',
+        releaseDateFirstEpisode: {
+            month: 'July',
+            year: 2016
+        },
+        createdBy: 'The Duffer Brothers',
+        seasons: '4',
+        episodes: '34',
+        genre: 'Horror, Science fiction, Drama',
+        ageRate: '16+',
+        trailer: 'https://www.youtube.com/watch?v=d0JYlUTbv1A',
+        images: {
+            poster1: 'https://i5.walmartimages.com/asr/c5217a96-aafc-4626-a702-b1f1db82afda.4e8ac9a9c7185fed7ba629dde0174a7e.jpeg',
+            poster2: 'https://files.ekmcdn.com/allwallpapers/images/stranger-things-4-you-will-lose-poster-61x91-5cm-new-sealed-37736-1-p.jpg',
+            poster3: 'https://i5.walmartimages.com/seo/Netflix-Stranger-Things-Bikes-Wall-Poster-22-375-x-34_4f4789af-1a21-43bb-933e-3a82f8bac7b2.5950f5ad148d4a3e80f8394d109a39af.jpeg',
+            posterH: 'https://static.displate.com/1200x857/displate/2022-09-05/67f549944a390c766fd186979ddb0f97_76ae2aa56db908b08a17c67a7371903a.jpg',
+        }
+    },
+    {
+        id: 13,
+        category: 'serie',
+        name: 'Peacemaker',
+        description: 'A DC series that follows Peacemaker as he fights for peace at any cost, no matter how many people he has to kill to get it.',
+        releaseDateFirstEpisode: {
+            month: 'January',
+            year: 2022
+        },
+        createdBy: 'James Gunn',
+        seasons: '1',
+        episodes: '8',
+        genre: 'Superhero, Comedy, Action',
+        ageRate: '18+',
+        trailer: 'https://www.youtube.com/watch?v=C37ih3OHxA0',
+        images: {
+            poster1: 'https://i.redd.it/ne2653ud3c381.jpg',
+            poster2: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e5e39892-db45-40a7-896e-b80c1867aa74/df6qouu-415608fa-fccf-4041-88bd-edef2207db8e.png/v1/fill/w_735,h_1088,q_70,strp/peacemaker_s1___poster_by_carpaa2011_df6qouu-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTc3NyIsInBhdGgiOiJcL2ZcL2U1ZTM5ODkyLWRiNDUtNDBhNy04OTZlLWI4MGMxODY3YWE3NFwvZGY2cW91dS00MTU2MDhmYS1mY2NmLTQwNDEtODhiZC1lZGVmMjIwN2RiOGUucG5nIiwid2lkdGgiOiI8PTEyMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.I2BJLdRz8sff0BeiGlNw6gkpvCLcaPHON1NkVLKHFac',
+            poster3: 'https://www.postersplug.com/cdn/shop/products/peacemaker-suicide-squad-movie-poster-674768.jpg?v=1692902240',
+            posterH: 'https://i0.wp.com/thefutureoftheforce.com/wp-content/uploads/2021/12/Peacemaker-Character-Posters-Header.jpg?fit=1920%2C1080&ssl=1',
+        }
+    },
+    {
+        id: 14,
+        category: 'serie',
+        name: 'Pretty Little Liars',
+        description: 'A group of teenage girls is stalked by a mysterious figure known as "A" after the disappearance of their friend, uncovering dark secrets along the way.',
+        releaseDateFirstEpisode: {
+            month: 'June',
+            year: 2010
+        },
+        createdBy: 'I. Marlene King',
+        seasons: '7',
+        episodes: '160',
+        genre: 'Mystery, Thriller, Drama',
+        ageRate: '14+',
+        trailer: 'https://www.youtube.com/watch?v=g2tlQS9zKvM',
+        images: {
+            poster1: 'https://i.ebayimg.com/00/s/MTYwMFgxMDY2/z/SAoAAOSwnT5ed9T-/$_57.JPG?set_id=8800005007',
+            poster2: 'https://m.media-amazon.com/images/I/71qg-LyOxGL.jpg',
+            poster3: 'https://i.redd.it/favourite-plls-poster-v0-4soct6i48kvb1.jpg?width=768&format=pjpg&auto=webp&s=6ee3ef8f521e3224ad9455e2caab4444e6b6d11a',
+            posterH: 'https://static0.cbrimages.com/wordpress/wp-content/uploads/2024/05/pretty-little-liars-ali-intro-season-1-7-posters.jpg',
+        }
+    },
+    {
+        id: 15,
+        category: 'serie',
+        name: 'Fallout',
+        description: 'Based on the popular video game series, Fallout is set in a post-apocalyptic world where survivors navigate dangerous wastelands and seek to rebuild society.',
+        releaseDateFirstEpisode: {
+            month: 'April',
+            year: 2024
+        },
+        createdBy: 'Graham Wagner; Geneva Robertson-Dworet',
+        seasons: '1',
+        episodes: '8',
+        genre: 'Science fiction, Action, Drama',
+        ageRate: '16+',
+        trailer: 'https://www.youtube.com/watch?v=GExflKHU3VE',
+        images: {
+            poster1: 'https://es.web.img3.acsta.net/pictures/24/03/08/10/09/2380565.jpg',
+            poster2: 'https://pbs.twimg.com/media/GKWJt6rWoAAIOTv.jpg:large',
+            poster3: 'https://pbs.twimg.com/media/GHxkomPX0AAp6AE.jpg:large',
+            posterH: 'https://i.blogs.es/1c2ac7/prime-video-fallout-2024/1366_2000.jpeg',
+        }
+    },
+
+]
