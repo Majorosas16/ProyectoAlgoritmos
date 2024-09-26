523bef466e8bee9cf2a206451cf786b033813146
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2024-09-25 22:00:08 -0500

    carrusel 3

--- src/components/Carrusel/carrusel.ts
+++ src/components/Carrusel/carrusel.ts
@@ -1,3 +1,5 @@
+import { imagenes } from "../../data/imagenesCarrusel";
+
 class Carousel extends HTMLElement {
 
     constructor() {
@@ -38,9 +40,46 @@ class Carousel extends HTMLElement {
 
             </section>
             </body>
-            
-              
             `;
+
+            let atras = document.getElementById('atras');
+            let adelante = document.getElementById('adelante');
+            let imagen = document.getElementById('img');
+            let puntos = document.getElementById('puntos');
+            let texto = document.getElementById('texto');
+            let actual = 0
+
+            atras?.addEventListener('click', function(){
+                actual -=1
+
+                if (actual == -1) {
+                    actual = imagenes.length - 1
+                }
+
+                imagen && (imagen.innerHTML = `<img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`);
+
+            })
+            adelante?.addEventListener('click', function(){
+                actual +=1
+
+                if (actual == imagenes.length) {
+                    actual = 0
+                }
+
+                imagen && (imagen.innerHTML = `<img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`);
+            })
+
+            function posicionCarrusel() {
+               
+                for (let i = 0; i < imagenes.length; i++) {
+                    if (i == actual) {
+                        puntos && (puntos.innerHTML += '<p class="bold>.</p>"');
+                    }
+                    else{
+                        puntos && (puntos.innerHTML += '<p>.</p>"');
+                    }
+                }
+            }
         }
     }
 }
