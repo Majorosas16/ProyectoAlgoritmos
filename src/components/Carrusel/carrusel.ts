7064e24aa37f33ec31cb86cd3ebfeff9be9d18a4
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2024-09-25 20:42:54 -0500

    carrusel 2

--- src/components/Carrusel/carrusel.ts
+++ src/components/Carrusel/carrusel.ts
@@ -13,26 +13,32 @@ class Carousel extends HTMLElement {
         if (this.shadowRoot) {
             this.shadowRoot.innerHTML = `
 
+            <link rel="stylesheet" href="../src/components/Carrusel/carrusel.css">
+
+            <body>
             <section class="carousel">
             <div class="atras">
-             <img id="atras" src="src/Recursos/Flecha (1).svg" alt="atras" loading="lazy">
+             <img id="atras" src="https://raw.githubusercontent.com/Majorosas16/ProyectoAlgoritmos/refs/heads/FirstDelivery/src/Recursos/Flecha%20(2).svg" alt="atras" loading="lazy">
             </div>
             
             <div class="imagenes">
             <div id="img">
-             <img id="img" src="src/Recursos/GOT.webp" alt="img" loading="lazy">
+             <img id="img" src="https://raw.githubusercontent.com/Majorosas16/ProyectoAlgoritmos/refs/heads/FirstDelivery/src/Recursos/Batman%20(1).webp" width="3000" alt="img" loading="lazy">
+
             </div>
             </div>
 
              <div id="texto" class="texto"></div>
 
              <div class="adelante" id="adelante" >
-             <img src="src/Recursos/Flecha (2).svg" alt="adelante" loading="lazy">
+             <img src="https://raw.githubusercontent.com/Majorosas16/ProyectoAlgoritmos/refs/heads/FirstDelivery/src/Recursos/Flecha%20(1).svg"  alt="adelante" loading="lazy">
             </div>
 
             <div class="puntos" id="puntos"></div>
 
             </section>
+            </body>
+            
               
             `;
         }
